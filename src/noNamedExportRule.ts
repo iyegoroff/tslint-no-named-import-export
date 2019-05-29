8dbce5bc75ded9fa577debe99cb56a9f488f07b2
Igor Yegorov <iegoroff@gmail.com>
2019-05-29 20:19:20 +0300

    fixes

--- src/noNamedExportRule.ts
+++ src/noNamedExportRule.ts
@@ -1,5 +1,7 @@
 import * as Lint from 'tslint'
 import * as ts from 'typescript'
+import flatten from 'array-flatten'
+import { basename } from 'path'
 
 export class Rule extends Lint.Rules.AbstractRule {
   public static metadata: Lint.IRuleMetadata = {
@@ -7,51 +9,84 @@ export class Rule extends Lint.Rules.AbstractRule {
     description: 'Disallows named exports in ES6-style modules.',
     descriptionDetails: 'Use default export instead.',
     rationale: 'Can be used to mimic `module interface` with default export.',
-    optionsDescription: 'Not configurable.',
-    options: null,
-    optionExamples: [true],
+    optionsDescription: 'A list of module names or regular expression patterns.',
+    options: {
+      type: 'array',
+      items: {
+        oneOf: [
+          {
+            type: 'string',
+            minLength: 1
+          },
+          {
+            type: 'array',
+            items: {
+              type: 'string'
+            },
+            minLength: 1
+          }
+        ]
+      }
+    },
+    optionExamples: [
+      true,
+      [true, 'view', 'state-\\d{2}'],
+      [true, 'state', ['^view\\w+', '^app$']]
+    ],
     type: 'maintainability',
     typescriptOnly: false
   }
 
-  public static FAILURE_STRING = 'Use of named exports is forbidden'
+  public isEnabled(): boolean {
+    return super.isEnabled() && this.ruleArguments.length > 0
+  }
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-    return this.applyWithFunction(sourceFile, walk)
+    return this.applyWithFunction(sourceFile, walk, this.ruleArguments)
   }
+
+  public static FAILURE_STRING = 'Use of named exports is forbidden'
 }
 
-function walk(ctx: Lint.WalkContext): void {
-  const syntaxList = (ctx.sourceFile.getChildren() || [])
+type Options = Array<string | string[]>
+
+function walk(ctx: Lint.WalkContext<Options>): void {
+  const { sourceFile } = ctx
+
+  if (
+    flatten<string>(ctx.options).some(p => new RegExp(p).test(basename(sourceFile.fileName)))
+  ) {
+    const syntaxList = (sourceFile.getChildren() || [])
     .find(node => node.kind === ts.SyntaxKind.SyntaxList)
 
-  if (syntaxList !== undefined) {
-    (syntaxList.getChildren() || []).forEach((node) => {
-      if (node.kind === ts.SyntaxKind.ExportKeyword) {
-        ctx.addFailureAtNode(node, Rule.FAILURE_STRING)
+    if (syntaxList !== undefined) {
+      (syntaxList.getChildren() || []).forEach((node) => {
+        if (node.kind === ts.SyntaxKind.ExportKeyword) {
+          ctx.addFailureAtNode(node, Rule.FAILURE_STRING)
+        }
+      })
+    }
+
+    sourceFile.statements.forEach((statement) => {
+      const { modifiers = [] } = statement
+
+      if (statement.kind === ts.SyntaxKind.ExportDeclaration) {
+        const { exportClause = { elements: [] } } = statement as ts.ExportDeclaration
+
+        if (!(
+          exportClause.elements.length === 1 &&
+          exportClause.elements[0].name.escapedText === 'default'
+        )) {
+          ctx.addFailureAtNode(statement.getChildAt(0, sourceFile), Rule.FAILURE_STRING)
+        }
+
+      } else if (
+        modifiers.length >= 1 &&
+        modifiers[0].kind === ts.SyntaxKind.ExportKeyword &&
+        (modifiers.length < 2 || modifiers[1].kind !== ts.SyntaxKind.DefaultKeyword)
+      ) {
+        ctx.addFailureAtNode(statement.getChildAt(0, sourceFile), Rule.FAILURE_STRING)
       }
     })
   }
-
-  ctx.sourceFile.statements.forEach((statement) => {
-    const { modifiers = [] } = statement
-
-    if (statement.kind === ts.SyntaxKind.ExportDeclaration) {
-      const { exportClause = { elements: [] } } = statement as ts.ExportDeclaration
-
-      if (!(
-        exportClause.elements.length === 1 &&
-        exportClause.elements[0].name.escapedText === 'default'
-      )) {
-        ctx.addFailureAtNode(statement.getChildAt(0, ctx.sourceFile), Rule.FAILURE_STRING)
-      }
-
-    } else if (
-      modifiers.length >= 1 &&
-      modifiers[0].kind === ts.SyntaxKind.ExportKeyword &&
-      (modifiers.length < 2 || modifiers[1].kind !== ts.SyntaxKind.DefaultKeyword)
-    ) {
-      ctx.addFailureAtNode(statement.getChildAt(0, ctx.sourceFile), Rule.FAILURE_STRING)
-    }
-  })
 }
